f9c0ae2cddc59c272c72b2fbdf5194e4eceb7151
sori883 <service@dlme.biz>
2020-06-01 00:04:23 +0900

    error_listをinclude

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,6 +9,7 @@
 
         <div class="mx-auto col-12 col-sm-11 col-md-9 col-lg-7 col-xl-6">
             <h2 class="card-title text-center">Laratodo</h5>
+            @include('error_list')
             <div class="card">
                 <div class="card-body">
 
@@ -16,12 +17,12 @@
                         @csrf
                         <div class="form-group">
                             <label for="usernameInput">ユーザ名</label>
-                            <input type="email" class="form-control" id="usernameInput" name="name" aria-describedby="userHelp" placeholder="ユーザ名" value="{{ old('name') }}" required>
+                            <input type="text" class="form-control" id="usernameInput" name="name" placeholder="ユーザ名" value="{{ old('name') }}" required>
                             <small id="emailHelp" class="form-text text-muted">英数字3～16文字（登録後の変更は出来ません）</small>
                         </div>
                         <div class="form-group">
                             <label for="emailInput">メールアドレス</label>
-                            <input type="email" class="form-control" id="emailInput" name="email" aria-describedby="emailHelp" placeholder="メールアドレス" value="{{ old('email') }}" required>
+                            <input type="email" class="form-control" id="emailInput" name="email" placeholder="メールアドレス" value="{{ old('email') }}" required>
                         </div>
                         <div class="form-group">
                             <label for="passwordInput">パスワード</label>
@@ -31,7 +32,7 @@
                             <label for="passwordConfirmInput">パスワード（確認）</label>
                             <input type="password" class="form-control" id="passwordConfirmInput" name="password_confirmation" placeholder="パスワード（確認）" required>
                         </div>
-                        <button type="button" class="btn btn-primary btn-lg btn-block" type="submit">登録</button>
+                        <button class="btn btn-primary btn-lg btn-block" type="submit">登録</button>
                     </form>
 
                 </div>
