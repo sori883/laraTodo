12b83aae12e5fcf18e654cae41398137d7c35730
sori883 <service@dlme.biz>
2020-05-31 20:22:22 +0900

    create view

---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,13 @@
+<nav class="navbar navbar-dark bg-dark navbar-expand-md">
+    <a class="navbar-brand" href="{{ route('projects.index') }}">Laratodo</a>
+
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav ml-auto">
+            <a class="nav-item nav-link text-white" href="#">ログアウト</a>
+        </div>
+    </div>
+</nav>
