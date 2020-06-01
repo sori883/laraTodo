61e1c0343fd8a25eba94e784b0355a91d4e71b83
sori883 <service@dlme.biz>
2020-06-01 22:22:40 +0900

    自分のプロジェクトとタスクだけ取得するように変更

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -4,14 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Project;
 use App\Task;
+
+use Auth;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
     public function index()
     {
-        $projects = Project::all()->sortByDesc('created_at');
-        $tasks = Task::all()->sortByDesc('created_at');
+        // TODO ここVSCodeでエラー出る
+        $user = Auth::user();
+
+        $projects = $user->projects->sortByDesc('created_at');
+        $tasks = $user->tasks->sortByDesc('created_at');
 
         return view('Projects.index', [
             'projects' => $projects,
