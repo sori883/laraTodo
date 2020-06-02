9e948a415380721d433bbe06843aabaa5c41c6f1
sori883 <service@dlme.biz>
2020-06-02 23:24:21 +0900

    ユーザのテストデータ作成

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,6 +18,9 @@ use Illuminate\Support\Str;
 */
 
 $factory->define(User::class, function (Faker $faker) {
+
+    $emails = ['hoge@sori883.com', 'foo@sori883.com'];
+
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
