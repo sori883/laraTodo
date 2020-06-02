bbd514099d43a6d359930e9007cdeb3b7531e33c
sori883 <service@dlme.biz>
2020-06-02 23:25:45 +0900

    titleを10文字に変更 project_idを追加

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -7,10 +7,11 @@ use Faker\Generator as Faker;
 
 $factory->define(Task::class, function (Faker $faker) {
     return [
-        'title' => $faker->text,
+        'title' => $faker->realText(20),
         'status' => false,
         'limit_at' => $faker->date,
         'user_id' => 1,
+        'project_id' => 1,
         'created_at' => $faker->date,
         'updated_at' => $faker->date
     ];
