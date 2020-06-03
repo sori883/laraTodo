3b6b42a35d2df12bcdbaf4541966bd89a4b354f0
sori883 <service@dlme.biz>
2020-06-03 23:42:15 +0900

    projectsをprojectにリネーム

--- app/User.php
+++ app/User.php
@@ -38,7 +38,7 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function projects(): HasMany
+    public function project(): HasMany
     {
         return $this->hasMany('App\Project');
     }
