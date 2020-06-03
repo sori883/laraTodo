51485943473f662c84429b55cad2c1e0fbe37ab1
sori883 <service@dlme.biz>
2020-06-03 22:54:38 +0900

    projectsのルーティング追加

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'ProjectController@index')->name('projects.index');
-
 Auth::routes();
+
+Route::get('/', 'ProjectController@index')->name('projects.index');
+Route::resource('/projects', 'ProjectController')->except(['index']);
