f4efb0c4c66d3b55de55738c92a28596fbbe08cf
sori883 <service@dlme.biz>
2020-06-15 07:03:05 +0900

    リクエストのバリデーション作成

--- app/Http/Requests/ProjectRequest.php
+++ app/Http/Requests/ProjectRequest.php
@@ -13,7 +13,7 @@ class ProjectRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,14 @@ class ProjectRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'projectName' => 'required|max:20',
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
+            'projectName' => 'プロジェクト名',
         ];
     }
 }
