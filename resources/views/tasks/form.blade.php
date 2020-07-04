27316b285c02bd691b3c1b5bb98ee65a023d52c8
sori883 <service@dlme.biz>
2020-07-05 04:40:07 +0900

    タスク追加用フォームを作成

---
+++ resources/views/tasks/form.blade.php
@@ -0,0 +1,4 @@
+<div class="form-group mt-3">
+    <input type="text" class="form-control" id="taskInput" name="title" placeholder="タスク名" value="" required>
+    <button type="submits" class="btn btn-primary mt-1 btn-sm">タスクを追加</button>
+</div>
