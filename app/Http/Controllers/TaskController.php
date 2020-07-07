082346cfa0b7a0a7633032a5aa011a93b168a747
sori883 <service@dlme.biz>
2020-07-07 23:16:46 +0900

    storeがフロントから叩けるか確認

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Task;
 
 class TaskController extends Controller
 {
-    //
+
+    public function store(Task $task){
+        return redirect()->route('projects.index');
+    }
+
 }
