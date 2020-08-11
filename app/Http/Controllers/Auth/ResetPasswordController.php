05257d76d26a75f6f9c75fa3903511b27c1be6b1
sori883 <service@dlme.biz>
2020-08-11 21:31:03 +0900

    showResetFormとresetメソッドでCookie関連の処理追加

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Facades\Crypt;
 
 class ResetPasswordController extends Controller
 {
@@ -27,4 +30,72 @@ class ResetPasswordController extends Controller
      * @var string
      */
     protected $redirectTo = RouteServiceProvider::HOME;
+
+    /**
+     * Display the password reset view for the given token.
+     *
+     * If no token is present, display the link request form.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  string|null  $token
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function showResetForm($token = null, Request $request)
+    {
+        $email = $request->input('email');
+        return $this->redirectWithToken('password_reset', $token, $email);
+    }
+
+    /**
+     * Reset the given user's password.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
+     */
+    public function reset(Request $request)
+    {
+        $token = Crypt::decryptString($request->token);
+        $email = Crypt::decryptString($request->email);
+
+        $request->merge([
+            'token' => $token,
+            'email' => $email
+        ]);
+
+        // email判定があるので、復号後にバリデーションを実行
+        $request->validate($this->rules(), $this->validationErrorMessages());
+
+        // Here we will attempt to reset the user's password. If it is successful we
+        // will update the password on an actual user model and persist it to the
+        // database. Otherwise we will parse the error and return the response.
+        $response = $this->broker()->reset(
+            $this->credentials($request), function ($user, $password) {
+                $this->resetPassword($user, $password);
+            }
+        );
+
+        // If the password was successfully reset, we will redirect the user back to
+        // the application's home authenticated view. If there is an error we can
+        // redirect them back to where they came from with their error message.
+        return $response == Password::PASSWORD_RESET
+                    ? $this->sendResetResponse($request, $response)
+                    : $this->sendResetFailedResponse($request, $response);
+    }
+
+    /**
+     * redirect with token
+     *
+     * @param  string  $route
+     * @param  string  $message
+     * @param  string  $email
+     * @return Redirect
+     */
+    protected function redirectWithToken($vueRoute, $token, $email)
+    {
+        $baseUrl = config('app.url');
+        $route = "{$baseUrl}/{$vueRoute}";
+        return redirect($route)
+                            ->cookie('RESETTOKEN', $token, 0, '', '', false, false)
+                            ->cookie('EMAIL', $email, 0, '', '', false, false);
+    }
 }
