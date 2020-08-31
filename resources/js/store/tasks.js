36780c6e2672eb85d7753427780721fb9c3d0422
sori883 <service@dlme.biz>
2020-08-31 21:48:03 +0900

    selecttask追加

--- resources/js/store/tasks.js
+++ resources/js/store/tasks.js
@@ -1,15 +1,20 @@
 const state = {
-    tasks: null
+    tasks: null,
+    selectedTask: null
 }
 
 const getters = {
-    tasks: (state) => state.tasks
+    tasks: (state) => state.tasks,
+    selectedTask: (state) => state.selectedTask
 }
 
 const mutations = {
     setTasks (state, Tasks) {
         state.tasks = Tasks
     },
+    setSelectedTask(state, selectedTask) {
+        state.selectedTask = selectedTask
+    }
 }
 
 const actions = {
@@ -17,13 +22,13 @@ const actions = {
         const response = await axios.get('/api/tasks/index')
         context.commit('setTasks', response.data)
     },
-    async projectTasks (context, project_id) {
+    async TaskTasks (context, Task_id) {
         let response
 
-        if (typeof project_id === 'undefined') {
+        if (typeof Task_id === 'undefined') {
             response = await axios.get('/api/tasks/index')
         } else {
-            response = await axios.get(`/api/tasks/project/${project_id}`)
+            response = await axios.get(`/api/tasks/Task/${Task_id}`)
         }
 
         context.commit('setTasks', response.data)
