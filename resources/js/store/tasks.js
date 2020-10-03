a14f8ac9bb03a6ce0c21691342640c84f79afd88
sori883 <service@dlme.biz>
2020-10-03 16:33:53 +0900

    タスク削除関連の関数名を変更

--- resources/js/store/tasks.js
+++ resources/js/store/tasks.js
@@ -12,7 +12,7 @@ const mutations = {
     setTasks (state, Tasks) {
         state.tasks = Tasks
     },
-    setSelectedTask(state, selectedTask) {
+    setSelectedTask (state, selectedTask) {
         state.selectedTask = selectedTask
     }
 }
@@ -22,15 +22,19 @@ const actions = {
         const response = await axios.get('/api/tasks/index')
         context.commit('setTasks', response.data)
     },
-    async TaskTasks (context, Task_id) {
+    async projectTasks (context, Task_id) {
         let response
 
         if (typeof Task_id === 'undefined') {
             response = await axios.get('/api/tasks/index')
         } else {
-            response = await axios.get(`/api/tasks/Task/${Task_id}`)
+            response = await axios.get(`/api/tasks/project/${Task_id}`)
         }
 
+        context.commit('setTasks', response.data)
+    },
+    async delTask (context, TaskId) {
+        const response = await axios.delete(`/api/tasks/destroy/${TaskId}`)
         context.commit('setTasks', response.data)
     }
 }
