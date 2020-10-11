f947b1bef9ed84b1c0b90a75e939f732872d49ad
sori883 <service@dlme.biz>
2020-10-11 22:08:54 +0900

    期限をnull許容に変更

--- database/migrations/2020_05_31_085443_create_tasks_table.php
+++ database/migrations/2020_05_31_085443_create_tasks_table.php
@@ -17,7 +17,7 @@ class CreateTasksTable extends Migration
             $table->bigIncrements('id');
             $table->string('title',100);
             $table->boolean('status')->default(false);
-            $table->date('limit_at');
+            $table->date('limit_at')->nullable();
             $table->bigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->bigInteger('project_id')->nullable();
