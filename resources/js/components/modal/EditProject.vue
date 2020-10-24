5ef33e773e00c44599df6f02b0704b7fec307c56
sori883 <service@dlme.biz>
2020-10-25 01:04:52 +0900

    validate追加

--- resources/js/components/modal/EditProject.vue
+++ resources/js/components/modal/EditProject.vue
@@ -1,13 +1,25 @@
 <template>
-<b-modal id="projectEditModal" title="プロジェクト編集" @show="setProjectTitle">
-    <b-form-group id="project-edit-group" label="プロジェクト名" label-for="email">
-        <b-form-input id="title" v-model="projectForm.title" type="text" required placeholder="プロジェクト名"></b-form-input>
-    </b-form-group>
-    <template v-slot:modal-footer="{ cancel }">
-        <b-button variant="danger" @click="cancel()">キャンセル</b-button>
-        <b-button variant="success" @click="editProject">編集</b-button>
-    </template>
-</b-modal>
+<validation-observer ref="observer" v-slot="{ handleSubmit }">
+    <b-modal id="projectEditModal" title="プロジェクト編集" @show="setProjectTitle">
+        <validation-provider name="プロジェクト名" :rules="{ required: true, max: 20 }" v-slot="validationContext">
+            <b-form-group id="project-edit-group" label="プロジェクト名" label-for="email">
+                <b-form-input
+                    id="title"
+                    v-model="projectForm.title"
+                    type="text"
+                    :state="validationState(validationContext)"
+                    aria-describedby="titleFeedback"
+                    placeholder="プロジェクト名"
+                ></b-form-input>
+                <b-form-invalid-feedback id="titleFeedback">{{ validationContext.errors[0] }}</b-form-invalid-feedback>
+            </b-form-group>
+        </validation-provider>
+        <template v-slot:modal-footer="{ cancel }">
+            <b-button variant="danger" @click="cancel()">キャンセル</b-button>
+            <b-button variant="success" @click="handleSubmit(editProject)">編集</b-button>
+        </template>
+    </b-modal>
+</validation-observer>
 </template>
 
 <script>
@@ -31,6 +43,9 @@ export default {
         }
     },
     methods: {
+        validationState({ dirty, validated, valid = null }) {
+            return dirty || validated ? valid : null;
+        },
         setProjectTitle () {
             this.projectForm.title = this.selectedProject.title
         },
