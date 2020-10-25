83a7c2c146e3b0f985ff7315d323d517d5d821e7
sori883 <service@dlme.biz>
2020-10-25 23:42:09 +0900

    バリデーションの追加

--- resources/js/pages/PasswordReset.vue
+++ resources/js/pages/PasswordReset.vue
@@ -1,28 +1,44 @@
 <template>
 <b-col cols="12" sm="11" md="9" lg="7" xl="6" class="mx-auto">
-    <Error />
     <h2 class="text-center">パスワード再設定</h2>
     <b-card>
-        <b-form @submit="passwordReset">
-            <b-form-group id="password-group" label="パスワード" label-for="password">
-                <b-form-input id="password" v-model="PasswordResetForm.password" type="password" required placeholder="パスワード"></b-form-input>
-            </b-form-group>
-            <b-form-group id="password-confirm-group" label="パスワード（確認）" label-for="password-confirm">
-                <b-form-input id="password-confirm" v-model="PasswordResetForm.password_confirmation" type="password" required placeholder="パスワード（確認）"></b-form-input>
-            </b-form-group>
-            <b-button type="submit" block variant="primary">パスワードをリセット</b-button>
-        </b-form>
+        <validation-observer ref="observer" v-slot="{ handleSubmit }">
+            <validation-provider v-slot="validationContext" name="パスワード" :rules="{ required: true }" vid="confirmation">
+                <b-form-group id="password-group" label="パスワード" label-for="password">
+                    <b-form-input
+                        id="password"
+                        v-model="PasswordResetForm.password"
+                        type="password"
+                        placeholder="パスワード"
+                        :state="validationState(validationContext)"
+                        aria-describedby="passwordFeedback"
+                    ></b-form-input>
+                    <b-form-invalid-feedback id="passwordFeedback">{{ validationContext.errors[0] }}</b-form-invalid-feedback>
+                </b-form-group>
+            </validation-provider>
+
+            <validation-provider v-slot="validationContext" name="パスワード（確認）" :rules="{ required: true, confirmed: 'confirmation'}">
+                <b-form-group id="password-confirm-group" label="パスワード（確認）" label-for="password-confirm">
+                    <b-form-input
+                        id="password-confirm"
+                        v-model="PasswordResetForm.password_confirmation"
+                        type="password"
+                        placeholder="パスワード（確認）"
+                        :state="validationState(validationContext)"
+                        aria-describedby="passwordConfirmFeedback"
+                    ></b-form-input>
+                    <b-form-invalid-feedback id="passwordConfirmFeedback">{{ validationContext.errors[0] }}</b-form-invalid-feedback>
+                </b-form-group>
+            </validation-provider>
+            <b-button block variant="primary" @click="handleSubmit(passwordReset)">パスワードをリセット</b-button>
+        </validation-observer>
     </b-card>
 </b-col>
 </template>
 
 <script>
 import Cookies from 'js-cookie';
-import Error from '../components/Error.vue'
 export default {
-    components: {
-        Error
-    },
     data () {
         return {
             PasswordResetForm: {
@@ -33,21 +49,11 @@ export default {
             }
         }
     },
-    methods: {
-        async passwordReset(evt) {
-            evt.preventDefault()
-
-            await axios.post('/api/password/update', this.PasswordResetForm)
-                .catch((e) => {
-                    this.$store.commit('error/setmessage', e.response.data.errors)
-                })
-        }
-    },
     created() {
         const token = Cookies.get('RESETTOKEN');
         const email = Cookies.get('EMAIL');
         // tokenがnullの場合はTOPにリダイレクト
-        if (this.PasswordResetForm.token == null) {
+        if (typeof token === 'undefined') {
             this.$router.push({name: 'top'});
         }
 
@@ -57,6 +63,14 @@ export default {
         if (token) {
             Cookies.remove('RESETTOKEN');
             Cookies.remove('EMAIL');
+        }
+    },
+    methods: {
+        validationState({ dirty, validated, valid = null }) {
+            return dirty || validated ? valid : null;
+        },
+        passwordReset() {
+            this.$store.dispatch('auth/passwordReset', this.PasswordResetForm)
         }
     }
 }
