f15d586593b7b67c78d01cd68b3989d1ae3df325
sori883 <service@dlme.biz>
2020-10-26 22:34:54 +0900

    エラー表示用コンポーネント再使用

---
+++ resources/js/components/Error.vue
@@ -0,0 +1,26 @@
+<template>
+<div v-show="messages" class="mt-3">
+    <b-alert show variant="danger">
+        <ul class="text-left">
+            <li v-for="value in messages" :key="value">
+                {{ value }}
+            </li>
+        </ul>
+    </b-alert>
+</div>
+</template>
+
+<script>
+export default {
+    computed: {
+        messages () {
+            return this.$store.getters['error/message']
+        }
+    },
+    methods: {
+        async deleteMessages () {
+            await this.$store.commit('error/deletemessages')
+        }
+    }
+}
+</script>
