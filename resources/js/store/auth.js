705eb28b910558a35b51920d1277eaffef2d8bb9
sori883 <service@dlme.biz>
2020-10-27 05:37:06 +0900

    登録時とログイン時にstateに値が入らない問題を解消

--- resources/js/store/auth.js
+++ resources/js/store/auth.js
@@ -3,6 +3,7 @@ const state = {
 }
 
 const getters = {
+    user: (state) => state.user,
     isLogin: (state) => !! state.user,
 }
 
@@ -14,27 +15,27 @@ const mutations = {
 
 const actions = {
     async register (context, data) {
-        const response = await axios.post('/api/user/register', data)
-        context.commit('setUser', response.data)
+        await axios.post('/api/user/register', data)
+        await context.dispatch('loginUser')
     },
     async login (context, data) {
-        const response = await axios.post('/api/user/login', data)
-        context.commit('setUser', response.data)
+        await axios.post('/api/user/login', data)
+        await context.dispatch('loginUser')
     },
     async logout (context) {
         await axios.post('/api/user/logout')
         context.commit('setUser', null)
-    },
-    async loginUser (context) {
-        const response = await axios.get('/api/user')
-        const user = response.data || null
-        context.commit('setUser', user)
     },
     async passwordConfirm (context, data) {
         await axios.post('/api/password/email', data)
     },
     async passwordReset (context, data) {
         await axios.post('/api/password/update', data)
+    },
+    async loginUser (context) {
+        const response = await axios.get('/api/user')
+        const user = response.data || null
+        context.commit('setUser', user)
     }
 }
 
