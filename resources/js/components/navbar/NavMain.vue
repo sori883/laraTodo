9ec46c706c0f59d602db3765036be1fa14e7f46b
sori883 <service@dlme.biz>
2020-11-25 22:57:45 +0900

    toggleボタンクリック時にsidebarのstateを変更させた

--- resources/js/components/navbar/NavMain.vue
+++ resources/js/components/navbar/NavMain.vue
@@ -1,7 +1,14 @@
 <template>
 <b-navbar tag="header" toggleable="lg" type="dark" class="navbar-main">
-    <awesome-icon :icon="['fas', 'bars']" class="toggle-sidebar-icon" />
+    <awesome-icon :icon="['fas', 'bars']" @click="toggleActive" class="toggle-sidebar-icon" />
 </b-navbar>
 </template>
 <script>
+export default {
+    methods: {
+        toggleActive () {
+            this.$store.commit('visual/toggleSidebar')
+        }
+    }
+}
 </script>
