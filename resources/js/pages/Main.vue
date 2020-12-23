58071effe421baeada0be0d6c1d4c02ccb1ec1f5
sori883 <service@dlme.biz>
2020-12-23 22:46:05 +0900

    bootstrap modalを開いた時にスクロールバーがなくてもpadding-right:15px;がbodyに追加される問題を修正

--- resources/js/pages/Main.vue
+++ resources/js/pages/Main.vue
@@ -1,5 +1,5 @@
 <template>
-<main>
+<div>
     <NavMain />
     <div class="wrapper">
         <ProjectList />
@@ -16,7 +16,7 @@
             <awesome-icon :icon="['fas', 'plus']" />
         </b-button>
     </div>
-</main>
+</div>
 </template>
 
 <script>
@@ -31,6 +31,8 @@ import TaskList from '../components/TaskList.vue'
 import DelTask from '../components/modal/DelTask.vue'
 import CreateTask from '../components/modal/CreateTask.vue'
 import EditTask from '../components/modal/EditTask.vue'
+
+import { getScrolllbarWidth } from '../mixins/fix.js'
 export default {
     components: {
         NavMain,
@@ -42,6 +44,17 @@ export default {
         DelTask,
         CreateTask,
         EditTask
-    }
+    },
+    mounted() {
+        if (getScrolllbarWidth() === 0) {
+            // スクロールバーが表示されていない状態でpadding-rightが表示される現象を修正
+            this.$root.$on('bv::modal::show', () => {
+                document.body.classList.add('fix-modal-padding')
+            })
+            this.$root.$on('bv::modal::hidden', () => {
+                document.body.classList.remove('fix-modal-padding')
+            })
+        }
+    },
 }
 </script>
