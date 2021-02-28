049d97df96a8c517c27ffcce563f03ee52f8513d
sori883 <service@dlme.biz>
2021-02-28 23:01:56 +0900

    ProjectRequestのテスト修正

--- tests/Request/ProjectRequestTest.php
+++ tests/Request/ProjectRequestTest.php
@@ -1,22 +1,37 @@
 <?php
 
-namespace Tests\Feature;
+namespace Tests\Request;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Validator;
+use App\Http\Requests\ProjectRequest;
 use Tests\TestCase;
 
 class ProjectRequestTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $response = $this->get('/');
 
-        $response->assertStatus(200);
+    /**
+     * @dataProvider dataproviderProjectForm
+     */
+    public function testProjectFormValidation($item, $data, $expect)
+    {
+        $dataList = [$item => $data];
+        $request = new ProjectRequest();
+
+        $rules = $request->rules();
+        $validator = Validator::make($dataList, $rules);
+        $result = $validator->passes();
+
+        $this->assertEquals($expect, $result);
     }
+
+  public function dataproviderProjectForm()
+  {
+      return [
+          '正常' => ['title', str_repeat('a', 20), true],
+          '必須エラー' => ['title', '', false],
+          '最大文字数エラー' => ['title', str_repeat('a', 21), false],
+      ];
+  }
 }
