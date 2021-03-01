6453eb4146c38d2f11daf3b9ed6191bf92271703
sori883 <service@dlme.biz>
2021-03-01 21:59:43 +0900

    タスクリクエストテスト完了

--- tests/Request/TaskRequestTest.php
+++ tests/Request/TaskRequestTest.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Validator;
 use App\Http\Requests\TaskRequest;
+use Carbon\Carbon;
 use Tests\TestCase;
 
 class TaskRequestTest extends TestCase
@@ -13,13 +14,11 @@ class TaskRequestTest extends TestCase
     /**
      * @dataProvider dataproviderTaskForm
      */
-    public function testTaskFormValidation($item, $data, $expect)
+    public function testTaskFormValidation($data, $expect)
     {
-        $dataList = [$item => $data];
-        $request = new TaskRequest();
 
-        $rules = $request->rules();
-        $validator = Validator::make($dataList, $rules);
+        $rules = (new TaskRequest())->rules();
+        $validator = Validator::make($data, $rules);
         $result = $validator->passes();
 
         $this->assertEquals($expect, $result);
@@ -27,11 +26,34 @@ class TaskRequestTest extends TestCase
 
   public function dataproviderTaskForm()
   {
-      return [
-          '正常' => ['title', str_repeat('a', 50), true],
-          '正常' => ['title', str_repeat('a', 50), true],
-          '必須エラー' => ['title', '', false],
-          '最大文字数エラー' => ['title', str_repeat('a', 21), false],
-      ];
+    return [
+        [
+            '正常' => [
+                'title' => str_repeat('a', 50),
+                'limit_at' => Carbon::now()->format('Y/m/d H:i'),
+                'project_id' => ''
+            ], true,
+        ],
+        [
+            '必須エラー' => [
+                'title' => ''
+            ], false,
+        ],
+        [
+            '最大文字数エラー' => [
+                'title' => str_repeat('a', 51)
+            ], false
+        ],
+        [
+            '日付けエラー' => [
+                'limit_at' => Carbon::now()->format('Y/m/d H:i:s'),
+            ], false,
+        ],
+        [
+            'プロジェクトエラー' => [
+                'limit_at' => 'test',
+            ], false,
+        ],
+    ];
   }
 }
