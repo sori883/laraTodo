854be8b4b7e7996dde7db1fcf77d67a7cf50d6eb
sori883 <service@dlme.biz>
2021-03-10 22:05:01 +0900

    ユーザのエラー系テスト完了

--- tests/Feature/UserControllerErrorTest.php
+++ tests/Feature/UserControllerErrorTest.php
@@ -2,21 +2,79 @@
 
 namespace Tests\Feature;
 
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Illuminate\Support\Facades\Notification;
 
 class UserControllerErrorTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
+    use WithFaker;
 
-        $response->assertStatus(200);
+    protected $user;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->user = factory(User::class)->create();
+    }
+
+    public function testValidRegister(): void
+    {
+        $response = $this->json('post', route('user.register'), [
+            'name' => str_repeat('a', 2),
+            'email' => 'not email',
+            'password' => str_repeat('a', 7),
+            'password_confirmation' => str_repeat('a', 7),
+        ]);
+
+        $response
+        ->assertStatus(422)
+        ->assertJsonCount(3, 'errors');
+    }
+
+    public function testDuplicateRegister(): void
+    {
+        $response = $this->json('post', route('user.register'), [
+            'name' => $this->user->name,
+            'email' => $this->user->email,
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ]);
+
+        $response
+        ->assertStatus(422)
+        ->assertJsonCount(2, 'errors');
+    }
+
+    public function testNotRegistedUserLogin(): void
+    {
+        $response = $this->json('post', route('user.login'), [
+            'email' => 'not email',
+            'password' => str_repeat('a', 7),
+        ]);
+
+        $response
+        ->assertStatus(422)
+        ->assertJsonCount(1, 'errors');
+    }
+
+    public function testNotRegistedSendPasswordConfirmMail(): void
+    {
+        Notification::fake();
+
+        $email = $this->faker->unique()->safeEmail();
+
+        $response = $this->json('post', route('password.email'), [
+            'email' => $email,
+        ]);
+
+        $response
+            ->assertStatus(422)
+            ->assertJsonCount(1, 'errors');
+
+            Notification::assertNothingSent();
     }
 }
